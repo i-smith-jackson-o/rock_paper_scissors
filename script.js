d32f6ce88a3c35ca101bf2f352ea61d0b82a6956
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2022-07-10 23:34:57 -0400

    refactored to remove unnecessary functions

--- script.js
+++ script.js
@@ -3,63 +3,35 @@ let objects = ['Rock','Paper','Scissors'];
 let computerSelection = 0; 
 let playerSelection = 0; 
 
-// Randomly generate number between 0-2 
-function randomNumber(min=0, max=3) {
-  const r = Math.random()*(max-min) + min; 
-  return Math.floor(r); 
-}
+const rock = document.getElementById("0");
+const paper = document.getElementById("1");
+const scissors = document.getElementById("2");
 
-// Computer's round.
-function computerPlay(computerSelection) {
-  let i = randomNumber(); 
-  computerSelection = objects[i];
-  return computerSelection;
-}
-
-// Player round
-function playerPlay(playerSelection) {
-  return objects[playerSelection];
-}
-
-// rock 
-const btnA = document.querySelector('#rock'); 
-btnA.addEventListener('click', function(e) {
-  playRound(computerSelection, playerSelection=0) //rock
-});
-
-// paper
-const btnB = document.querySelector('#paper'); 
-btnB.addEventListener('click', function(e) {
-  playRound(computerSelection, playerSelection=0) //rock
-});
-
-//scissors
-const btnC = document.querySelector('#scissors'); 
-btnC.addEventListener('click', function(e) {
-  playRound(computerSelection, playerSelection=0) //rock
-});
-
-function playRound(computerSelection, playerSelection) {
+rock.addEventListener("click",function(e){
   computerSelection = computerPlay(); 
-  playerSelection = playerPlay(playerSelection);
-  //playerSelection = objects[playerSelection]; 
-  console.log(`Computer played ${computerSelection}.`); 
-  console.log(`User played ${playerSelection}.`);
+  playerSelection = objects[0];
+  playRound(computerSelection, playerSelection); 
+});
+paper.addEventListener("click",function(e){
+  computerSelection = computerPlay(); 
+  playerSelection = objects[1];
+  playRound(computerSelection, playerSelection); 
+});
+scissors.addEventListener("click",function(e){
+  computerSelection = computerPlay(); 
+  playerSelection = objects[2];
+  playRound(computerSelection, playerSelection); 
+});
 
-  if (computerSelection === playerSelection){
-    console.log(`Draw: ${computerSelection} === ${playerSelection}.`);
-  }
-  else if (computerSelection == 'rock' && playerSelection != 'paper') {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-  }
-  else if (computerSelection == 'paper' && playerSelection != 'scissors') {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-  }
-  else if (computerSelection == 'scissors' && playerSelection != 'rock') {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-  }
-  else {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-  }
+// Get computer selection 
+function computerPlay() {
+  const r = Math.random()*(3-1) + 1;
+  let i = Math.floor(r);
+  return objects[i];
 }
 
+// Game Logic
+function playRound(computerSelection, playerSelection) {
+  console.log(computerSelection); 
+  console.log(playerSelection);
+}
