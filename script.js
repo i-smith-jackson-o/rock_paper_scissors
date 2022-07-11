711819356ff933c4f69311d3bacea7d8d7fb1187
i-smith-jackson-o <i.smith.jackson.o@gmail.com>
2022-07-11 00:08:39 -0400

    finished console game logic

--- script.js
+++ script.js
@@ -9,29 +9,55 @@ const scissors = document.getElementById("2");
 
 rock.addEventListener("click",function(e){
   computerSelection = computerPlay(); 
-  playerSelection = objects[0];
+  playerSelection = 0;
   playRound(computerSelection, playerSelection); 
 });
 paper.addEventListener("click",function(e){
   computerSelection = computerPlay(); 
-  playerSelection = objects[1];
+  playerSelection = 1;
   playRound(computerSelection, playerSelection); 
 });
 scissors.addEventListener("click",function(e){
   computerSelection = computerPlay(); 
-  playerSelection = objects[2];
+  playerSelection = 2;
   playRound(computerSelection, playerSelection); 
 });
 
 // Get computer selection 
 function computerPlay() {
-  const r = Math.random()*(3-1) + 1;
+  const r = Math.random()*(3-0) + 0;
   let i = Math.floor(r);
-  return objects[i];
+  return i;
 }
 
 // Game Logic
 function playRound(computerSelection, playerSelection) {
-  console.log(computerSelection); 
-  console.log(playerSelection);
+  console.log("computer: " + objects[computerSelection]);
+  console.log("player: " + objects[playerSelection]);
+
+  if (computerSelection === playerSelection) {
+    console.log('Draw');
+    return;
+  }
+  if (computerSelection == 0) {
+    if (playerSelection == 1 ) {
+      console.log('player++');
+    } else {
+      console.log('computer++')
+    }
+  }
+  if (computerSelection == 1) {
+    if (playerSelection == 2 ) {
+      console.log('player++');
+    } else {
+      console.log('computer++')
+    }
+  }
+  if (computerSelection == 2) {
+    if (playerSelection == 0 ) {
+      console.log('player++');
+    } else {
+      console.log('computer++')
+    }
+  }
 }
